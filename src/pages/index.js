a204565ac208f88873a5bc4c9af9b038f54e63f4
Babak Chehraz <bchehraz@csumb.edu>
2020-02-07 16:03:40 -0800

    Refactored code into 2 new components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,80 +1,12 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
 import styled from "styled-components"
 
 import Layout from "components/layout"
 import SEO from "components/seo"
-import BgImage from "components/BgImage"
-import Button from "components/Button"
-import Container from "../components/Container"
-import ReactLogo from "../images/react-icon.png"
-import GatsbyLogo from "../images/gatsby-icon.png"
-import GraphQLLogo from "../images/graphql-icon.png"
-import NodeJSLogo from "../images/nodejs-icon.png"
-import JSLogo from "../images/js-icon.png"
-
-const StackImages = {
-  maxWidth: `15vh`,
-  padding: `10px`,
-}
-
-const LandingContainer = styled(`div`)`
-  position: relative;
-  display: flex;
-  flex-flow: column nowrap;
-  justify-content: space-around;
-  align-items: center;
-  height: 437px;
-  padding: 3rem 0 0;
-
-  h1,
-  h2 {
-    color: #ffffff;
-    font-size: 29px;
-    margin: 0 auto;
-    text-align: center;
-    padding: 0 15px;
-  }
-
-  h2 {
-    font-size: 21px;
-  }
-`
-
-const ImageCover = styled(`div`)`
-  position: absolute;
-  top: 0;
-  height: 437px;
-  width: 100vw;
-  z-index: -1;
-  background: rgb(22, 42, 62, 0.44);
-  background: -moz-linear-gradient(
-    0deg,
-    rgba(22, 42, 62, 1) 0%,
-    rgba(22, 42, 62, 0.44) 100%
-  );
-  background: -webkit-linear-gradient(
-    0deg,
-    rgba(22, 42, 62, 1) 0%,
-    rgba(22, 42, 62, 0.44) 100%
-  );
-  background: linear-gradient(
-    0deg,
-    rgba(22, 42, 62, 1) 0%,
-    rgba(22, 42, 62, 0.44) 100%
-  );
-  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#162a3e",endColorstr="#162a3e",GradientType=1);
-`
-
-const ButtonsContainer = styled(`div`)`
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  margin: 0 auto;
-  width: 100%;
-`
+import Container from "components/Container"
+import LandingSection from "components/LandingSection"
+import TechIcons from "components/TechIcons"
 
 const Bar = styled(`div`)`
   background: #162a3e;
@@ -86,63 +18,15 @@ const Bar = styled(`div`)`
   ${({ right }) => right && "border-radius: 0 0 0 300px;"}
 `
 
-const IconsContainer = styled(`div`)`
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  width: 100%;
-
-  img {
-    max-height: 100px;
-    margin: 0 auto;
-  }
-`
-
-const IconWrapper = styled(`div`)`
-  display: flex;
-  flex-flow: column nowrap;
-  justify-content: center;
-  align-items: center;
-  background-color: rgb(22, 42, 62, 1);
-  padding: 5px 5px 15px;
-  color: white;
-  width: 120px;
-  margin: 5px;
-  font-size: 18px;
-  font-weight: 400;
-  border-radius: 15px;
-`
-
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <section>
-      <LandingContainer>
-        <BgImage
-          fluid={data.homeBgImage.childImageSharp.fluid}
-          style={{
-            height: "437px",
-          }}
-        />
-        <ImageCover />
-        <h1>Modern, Fast, Responsive Websites</h1>
-        <h2>Based in Fresno, we’re ready to build your next web project</h2>
-        <ButtonsContainer>
-          <Link to="/services" style={{ padding: 5 }}>
-            <Button>Our Services</Button>
-          </Link>
-          <Link to="/contact" style={{ padding: 5 }}>
-            <Button secondary>Contact Us</Button>
-          </Link>
-        </ButtonsContainer>
-      </LandingContainer>
-    </section>
+    <LandingSection image={data.homeBgImage.childImageSharp} />
 
-    <section style={{ position: "relative", width: "100%" }}>
+    <div style={{ position: "relative", width: "100%" }}>
       <Bar />
       <Bar right />
+
       <Container title="Modern Tech To Our Advantage">
         <p>
           Tech can only advance through better development practices. This is
@@ -151,32 +35,10 @@ const IndexPage = ({ data }) => (
           and applications.
         </p>
       </Container>
-    </section>
+    </div>
 
     <Container title="Core Technologies" secondary>
-      <IconsContainer>
-        <IconWrapper>
-          <img style={StackImages} src={ReactLogo} alt="React" />
-          React
-        </IconWrapper>
-        <IconWrapper>
-          <img style={StackImages} src={GatsbyLogo} alt="Gatsby" />
-          Gatsby
-        </IconWrapper>
-        <IconWrapper>
-          <img style={StackImages} src={GraphQLLogo} alt="GraphQL" />
-          GraphQL
-        </IconWrapper>
-        <IconWrapper>
-          <img style={StackImages} src={NodeJSLogo} alt="NodeJS" />
-          NodeJS
-        </IconWrapper>
-        <IconWrapper>
-          <img style={StackImages} src={JSLogo} alt="JavaScript" />
-          JavaScript
-        </IconWrapper>
-      </IconsContainer>
-      <div></div>
+      <TechIcons />
     </Container>
 
     {/* 
